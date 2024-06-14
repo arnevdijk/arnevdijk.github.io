e59f3ee07e2e7460b0d2956c40d57e53f48e1f45
arnevdijk <arnevdijk11@gmail.com>
2024-06-14 09:34:59 +0200

    minor fix

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  stats: {
+    errorDetails: true,
+    children: true
+  }
+};
